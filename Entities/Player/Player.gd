d3f99d654b4e19e5cb8fb86554b3a1d0d7a99d08
Ondrej Schlaichert <schlaichert.o@gmail.com>
2023-03-06 20:11:09 +0100

    Added wolf, flashlight, adjusted stuff

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -1,10 +1,11 @@
 extends CharacterBody3D
 
-@export var speed := 5
-@export var jump_velocity := 4.5
+@export var walk_speed : float = 2.5
+@export var run_speed : float = 4
 
 # References to other nodes:
 @onready var camera : Camera3D = $Camera3D
+@onready var flashlight : SpotLight3D = $Camera3D/Flashlight
 
 var look_sensitivity : float = ProjectSettings.get_setting('player/look_sensitivity')
 var gravity : float = ProjectSettings.get_setting('physics/3d/default_gravity')
@@ -12,11 +13,16 @@ var velocity_y : float = 0
 
 
 func _physics_process(delta: float) -> void:
-	var horizontal_velocity := (
+	var horizontal_velocity : Vector2 = (
 		Input.get_vector(
 			'move_left', 'move_right', 'move_forward', 'move_backward'
-		).normalized() * speed
+		).normalized()
 	)
+
+	if not Input.is_action_pressed('run') or horizontal_velocity.y >= 0:
+		horizontal_velocity = horizontal_velocity * walk_speed
+	else:
+		horizontal_velocity = horizontal_velocity * run_speed
 
 	velocity = (
 		horizontal_velocity.x * global_transform.basis.x + 
@@ -25,7 +31,6 @@ func _physics_process(delta: float) -> void:
 
 	if is_on_floor():
 		velocity_y = 0
-		if Input.is_action_just_pressed('jump'): velocity_y = jump_velocity
 	else:
 		velocity_y -= gravity * delta
 
@@ -37,5 +42,8 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		rotate_y(- event.relative.x * look_sensitivity)
 		camera.rotate_x(- event.relative.y * look_sensitivity)
+
+	if event.is_action_pressed('toggle_flashlight'):
+		flashlight.visible = not flashlight.visible
 	
 	camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
---
+++ Entities/Wolf/Wolf.gd
@@ -0,0 +1,4 @@
+extends CharacterBody3D
+
+func _ready() -> void:
+	$AnimationPlayer.play('Wolf_Idle_')
